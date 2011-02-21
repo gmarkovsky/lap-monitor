1779323cdc7f1d81be0ccdb111cfda5f9d480f8a
gbmarkovsky@gmail.com <gbmarkovsky@gmail.com@86c6e37c-42c8-25e5-3a1e-d3ba90dc6b03>
2011-02-21 21:40:35 +0000

    Создан проект для Android

---
+++ trunk/src/com/gmail/gbmarkovsky/CheckPoints.java
@@ -0,0 +1,13 @@
+package com.gmail.gbmarkovsky;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CheckPoints extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ trunk/src/com/gmail/gbmarkovsky/TimeCheckPoint.java
@@ -0,0 +1,10 @@
+package com.gmail.gbmarkovsky;
+
+/**
+ * Контрольная точка по времени
+ * @author george
+ *
+ */
+public class TimeCheckPoint {
+
+}
