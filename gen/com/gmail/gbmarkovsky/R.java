b821001514d7085c691efd95537691004415eb04
gbmarkovsky <gbmarkovsky@gmail.com>
2011-02-23 22:05:58 +0000

    попытка создания ui

--- gen/com/gmail/gbmarkovsky/R.java
+++ gen/com/gmail/gbmarkovsky/R.java
@@ -13,11 +13,30 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int button1=0x7f050004;
+        public static final int button2=0x7f050007;
+        public static final int buttonTest=0x7f050009;
+        public static final int gridView12=0x7f050005;
+        public static final int radio0=0x7f050002;
+        public static final int radio1=0x7f050003;
+        public static final int radioGroup1=0x7f050001;
+        public static final int textView1=0x7f050000;
+        public static final int textView11=0x7f050006;
+        public static final int textViewTest=0x7f050008;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int editcheckpoint=0x7f030000;
+        public static final int main=0x7f030001;
+        public static final int testlay=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int back=0x7f040006;
+        public static final int byDistance=0x7f040004;
+        public static final int byTime=0x7f040003;
+        public static final int checkPointType=0x7f040002;
         public static final int hello=0x7f040000;
+        public static final int ok=0x7f040005;
     }
 }
